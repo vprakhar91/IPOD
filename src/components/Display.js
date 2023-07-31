48c516e2b4aa80df68fe07a76d49e8f60979af61
vprakhar91 <46259033+vprakhar91@users.noreply.github.com>
2023-07-31 17:46:17 +0530

    Add files via upload

---
+++ src/components/Display.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Menu from '../components/Menu';
+import Music from '../components/Music';
+import Songs from '../components/Songs';
+import Settings from '../components/Settings';
+import Playing from '../components/Playing';
+import "../css/Display.css"
+import Themes from './Themes';
+import WheelColor from './WheelColor';
+import LockScreen from './LockScreen';
+import Wallpaper from './Wallpaper';
+
+
+
+class Display extends React.Component {
+    render() {
+        const { active, currentMenu, menuItems, musicItems,songItems, playing, songIndex, audio, songUrl ,songImgUrl,wallpaper,wallpaperItems, noty, setNoty,notifyText} = this.props;
+        
+        return (
+            <div style={{backgroundImage:`url(${wallpaperItems[wallpaper]})`}} className="display">
+                <Navbar noty={noty} setNoty={setNoty} playing={playing} notifyText ={notifyText} />
+                {currentMenu===-2&&<LockScreen/>}
+                {currentMenu === -1 && <Menu songImgUrl={songImgUrl} menuItems={menuItems} active={active} />}
+                {currentMenu === 1 && <Music musicItems={musicItems} active={active} />}
+                {currentMenu === 2 && <div className="blank-div"><h1 className="empty-text">Games</h1></div>}
+                {currentMenu === 3 && <Settings active={active}/>}
+                {currentMenu === 4 && <Songs songItems={songItems} active={active} />}
+                {currentMenu === 5 && <div className="blank-div"><h1 className="empty-text">Artists</h1></div>}
+                {currentMenu === 6 && <div className="blank-div"><h1 className="empty-text">Albums</h1></div>}
+                {(currentMenu === 0 ||currentMenu===7) && <Playing songImgUrl={songImgUrl} audio={audio} songUrl={songUrl} playing={playing} songIndex={songIndex} songItems={songItems} />}
+                {currentMenu===8&&<Themes active={active}/>}
+                {currentMenu===9&&<WheelColor active={active}/>}
+                {currentMenu===10&&<Wallpaper active={active}/>}
+            </div>
+        )
+    }
+}
+
+
+export default Display;
---
+++ src/components/Music.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "../css/Music.css"
+
+class Music extends React.Component {
+    render() {
+        const {musicItems,active} = this.props;
+        return (
+            <div className="music">
+                <h3>Music</h3>
+                <ul>
+                {musicItems.map((element, index)=>{
+                            return active===index?<li key={index} className="active">&nbsp;{element}</li>:<li key={index}>&nbsp;{element}</li>
+                        })}
+                </ul>
+            </div>
+
+        )
+    }
+
+}
+
+
+export default Music;
